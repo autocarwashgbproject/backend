eab595d507bc9c080360b9e17cecda017e17c90d
nishadrin <nishadrin@gmail.com>
2019-08-30 03:00:20 +0300

    do permissions in client and washing

--- autocarwash/washing/permissions.py
+++ autocarwash/washing/permissions.py
@@ -1,16 +1,26 @@
 from rest_framework import permissions
 from re import split
 from rest_framework.authtoken.models import Token
+from client.models import User
 # from .models import
 
 
 class IsOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        print()
-        print(obj.phone)
-        print(type(obj.phone))
-        print()
-        print(request.user.phone)
-        print(type(request.user.phone))
-        print()
-        return bool(obj.phone == request.user.phone)
+    def has_permission(self, request, view):
+        # сверяем токен в бд у этого пользователя, с токеном, который пришел
+        token = request.headers['Authorization']
+        token = split(r' ', token)
+        token = token.pop()
+        pk = view.kwargs['pk']
+        try:
+            user = User.objects.get(id=pk)
+            inside_token = Token.objects.get(user=user)
+
+            if inside_token.key == token:
+                return True
+            else:
+                return False
+            return True
+            
+        except Exception as e:
+            return False
