72f535fa0413f9c7f82cf29b4b112a40e9675a65
domoboy <nkonoplyannikov@gmail.com>
2019-09-01 10:32:25 +0300

    добавил закомментированным регулярку для проверки валидности номера автомобиля

--- autocarwash/car/models.py
+++ autocarwash/car/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from subscription.models import Subscription
+from django.core.validators import RegexValidator
 
 User = get_user_model()
 
@@ -10,8 +11,13 @@ class Car(models.Model):
         verbose_name = 'Автомобиль'
         verbose_name_plural = "Автомобили"
 
+    # car_num_regex = RegexValidator(
+    #     regex=r'^[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]\d{2,3}$',
+    #     message='Неверный формат номера автомобиля'
+    # )
+
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    reg_num = models.CharField(verbose_name='Номер автомобиля', db_index=True, unique=True, max_length=9)
+    reg_num = models.CharField(verbose_name='Номер автомобиля', db_index=True, unique=True, max_length=9)  # validators=[car_num_regex]
     is_active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(verbose_name='Создан', auto_now_add=True)
     update_date = models.DateTimeField(verbose_name='Обновлен', auto_now=True)
