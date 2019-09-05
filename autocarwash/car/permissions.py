e4f69d8bfc0bffc610cde81d15aeb4b9ab94de8a
nishadrin <nishadrin@gmail.com>
2019-09-05 18:01:51 +0300

    fix permissions

--- autocarwash/car/permissions.py
+++ autocarwash/car/permissions.py
@@ -3,31 +3,36 @@ from re import split
 from rest_framework.authtoken.models import Token
 from client.models import User
 from car.models import Car
+from rest_framework.response import Response
 
 
 class IsOwner(permissions.BasePermission):
     def has_permission(self, request, view):
-        token = request.headers['Authorization']
-        token = split(r' ', token)
-        token = token.pop()
-        if request.method == "GET":
-            pk = view.kwargs['pk']
+        try:
+            token = request.headers['Authorization']
+            token = split(r' ', token)
+            token = token.pop()
+        except Exception as e:
+            pass
+        finally:
+            if request.method == "GET":
+                pk = view.kwargs['pk']
 
-            try:
-                car = Car.objects.get(id=pk)
-                user = car.user
-                inside_token = Token.objects.get(user=user)
+                try:
+                    car = Car.objects.get(id=pk)
+                    user = car.user
+                    inside_token = Token.objects.get(user=user)
 
-                if inside_token.key == token:
+                    if inside_token.key == token:
+                        return True
+                    else:
+                        return False
                     return True
-                else:
-                    return False
-                return True
 
-            except Exception as e:
-                return False
-        else:
-            return True
+                except Exception as e:
+                    return False
+            else:
+                return True
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
--- autocarwash/client/permissions.py
+++ autocarwash/client/permissions.py
@@ -2,26 +2,34 @@ from rest_framework import permissions
 from re import split
 from rest_framework.authtoken.models import Token
 from client.models import User
+from rest_framework.response import Response
 
 
 class IsOwner(permissions.BasePermission):
     def has_permission(self, request, view):
-        token = request.headers['Authorization']
-        token = split(r' ', token)
-        token = token.pop()
-        pk = view.kwargs['pk']
         try:
-            user = User.objects.get(id=pk)
-            inside_token = Token.objects.get(user=user)
-
-            if inside_token.key == token:
-                return True
-            else:
-                return False
-            return True
-
+            token = request.headers['Authorization']
+            token = split(r' ', token)
+            token = token.pop()
         except Exception as e:
-            return False
+            pass
+        finally:
+            pk = view.kwargs['pk']
+            try:
+                user = User.objects.get(id=pk)
+                inside_token = Token.objects.get(user=user)
+
+                if inside_token.key == token:
+                    return True
+                else:
+                    return False
+                return True
+
+            except Exception as e:
+                return False
 
     def has_object_permission(self, request, view, obj):
-        return bool(obj.phone == request.user.phone)
+        if obj != 'AnonymousUser':
+            return bool(obj.phone == request.user.phone)
+        else:
+            return False
